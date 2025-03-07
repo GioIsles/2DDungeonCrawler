31eb75e8439f4a1984f4af2442cec4a1468eeb60
gioisles <giovanni.raia1@marist.edu>
2025-03-06 19:42:44 -0500

    main

---
+++ Assets/scripts/PlayerMovement.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 1;
+
+    void Update()
+    {
+        Vector2 input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")).normalized;
+        transform.Translate(new Vector2(input.x, input.y) * speed * Time.deltaTime);
+    }
+}
